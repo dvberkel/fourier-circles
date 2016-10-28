c03aa047a45c153f6087cd03b3b955e9a11fceaa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-28 03:13:52 +0200

    Create a beginnerProgram

--- Main.elm
+++ Main.elm
@@ -1,4 +1,46 @@
-import Html exposing (text)
+import Html exposing (Html, div, span, text)
+import Html.App exposing (beginnerProgram)
 
+
+main : Program Never
 main =
-    text "Hello, World!"
+    beginnerProgram { model = model, view = view, update = update }
+
+
+type alias Model =
+    {
+        coefficients: List Int
+    }
+
+
+model : Model
+model =
+    { coefficients = [0] }
+
+
+type Message =
+    Nothing
+
+
+update : Message -> Model -> Model
+update message model =
+    model
+
+
+view : Model -> Html Message
+view model =
+    let
+        coefficients = viewCoefficients model.coefficients
+    in
+        div [] coefficients
+
+
+viewCoefficients : List Int -> List (Html Message)
+viewCoefficients coefficients =
+    List.map viewCoefficient coefficients
+
+
+viewCoefficient : Int -> Html Message
+viewCoefficient coefficient =
+    span [] [ text (toString coefficient) ]
+
